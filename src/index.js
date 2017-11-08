eddfda86526aa9f805782d685ed88a6d34b43387
Roni Choudhury <roni.choudhury@kitware.com>
2017-11-08 17:23:16 -0500

    Add a small dataset

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import smallData from '../data/jobmock-small.json';
+
 console.log('hello, world');
-document.write('<h1>hello, world</h1>');
+document.write(`<h1>hello, world</h1><pre>${JSON.stringify(smallData, null, 2)}</pre>`);
