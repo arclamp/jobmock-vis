3b01ccb827f7f8eb2f9c0062e321650ce286ef40
Roni Choudhury <roni.choudhury@kitware.com>
2017-11-10 16:50:22 -0500

    Add a mechanism to switch between vis/data choices

---
+++ src/data/index.js
@@ -0,0 +1,7 @@
+import jobmock6 from './jobmock6.json';
+import jobmock50 from './jobmock50.json';
+
+export {
+  jobmock6,
+  jobmock50
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 import { select } from 'd3-selection';
 
 import * as views from './vis';
-import jobmock6 from '../data/jobmock6.json';
-import jobmock50 from '../data/jobmock50.json';
+import * as data from './data';
 
 import content from './index.jade';
 
 select(document.body).html(content());
 
 const div = select('#vis').node();
+let vis;
 
-const vis = new views.TableView(div, {
-  data: jobmock6
-});
-vis.render();
+const updateVis = () => {
+  const whichView = getSelectedOption('select.visualization');
+  const whichData = getSelectedOption('select.data');
+
+  const View = views[whichView];
+  const dataset = data[whichData];
+
+  vis = new View(div, {
+    data: dataset
+  });
+  vis.render();
+};
+
+const makeMenu = (selector, data) => select(selector)
+  .on('change', updateVis)
+  .selectAll('option')
+  .data(data)
+  .enter()
+  .append('option')
+  .attr('value', d => d)
+  .text(d => d);
+
+const getSelectedOption = (selector) => {
+  const menu = select(selector).node();
+  return menu.options[menu.options.selectedIndex].value;
+};
+
+makeMenu('select.visualization', Object.keys(views));
+makeMenu('select.data', Object.keys(data));
+
+updateVis();
