db8efb6e97c45c43f1cca83877e19238b3e2b634
Roni Choudhury <roni.choudhury@kitware.com>
2017-11-13 18:53:25 -0500

    Add basic draggable node-link diagram

--- src/vis/NodeLink.js
+++ src/vis/NodeLink.js
@@ -1,3 +1,5 @@
+import * as d3 from 'd3';
+import { d3adaptor } from 'webcola';
 import { select } from 'd3-selection';
 import VisComponent from 'candela/VisComponent';
 
@@ -9,6 +11,9 @@ export default class NodeLink extends VisComponent {
 
     this.data = options.data;
 
+    this.width = options.width || 960;
+    this.height = options.height || 540;
+
     const graphData = graphStructure(this.data);
     this.node = graphData.node;
     this.link = graphData.link;
@@ -17,11 +22,60 @@ export default class NodeLink extends VisComponent {
       .selectAll('*')
       .remove();
 
-    this.pre = select(this.el).append('pre');
+    this.svg = select(this.el).append('svg');
   }
 
   render () {
-    const text = `${JSON.stringify(this.node, null, 2)}\n\n${JSON.stringify(this.link, null, 2)}`;
-    this.pre.text(text);
+    const cola = d3adaptor(d3)
+      .linkDistance(120)
+      .avoidOverlaps(true)
+      .size([this.width, this.height]);
+
+    this.svg.attr('width', this.width)
+      .attr('height', this.height);
+
+    cola.nodes(this.node)
+      .links(this.link)
+      .start();
+
+    let link = this.svg.selectAll('.link')
+      .data(this.link);
+    link = link.enter()
+      .append('line')
+      .classed('link', true)
+      .style('stroke', '#999')
+      .style('stroke-width', '3px')
+      .style('stroke-opacity', 1)
+      .merge(link);
+
+    const rectWidth = 100;
+    const rectHeight = 50;
+
+    let node = this.svg.selectAll('.node')
+      .data(this.node);
+    node = node.enter()
+      .append('rect')
+      .classed('node', true)
+      .style('stroke', 'black')
+      .style('stroke-width', '1.5px')
+      .style('cursor', 'move')
+      .attr('width', rectWidth)
+      .attr('height', rectHeight)
+      .attr('rx', 5)
+      .attr('ry', 5)
+      .style('fill', 'firebrick')
+      .merge(node);
+
+    node.call(cola.drag);
+
+    cola.on('tick', () => {
+      link.attr('x1', d => d.source.x)
+        .attr('y1', d => d.source.y)
+        .attr('x2', d => d.target.x)
+        .attr('y2', d => d.target.y);
+
+      node.attr('x', d => d.x - rectWidth / 2)
+        .attr('y', d => d.y - rectHeight / 2);
+    });
   }
 }
